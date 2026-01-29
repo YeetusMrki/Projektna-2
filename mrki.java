61c981c40a0b8b2ba90e22e10760abfefec23f9b
YeetusMrki <amelmrkaljevic06@gmail.com>
2026-01-29 16:19:46 +0100

    Add files via upload

---
+++ mrki.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class mrki {
+    public static void main(String[] args) {
+        /*1. Naloga Napiši programe, ki izpišejo na zaslon zvezdice ob vnosu določenega celega števila  Primer ob vnosu števila 5:*/
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vnesi celo število:");
+        int x = sc.nextInt();
+
+        /*for(int i = 0; i < x; i++){
+            for(int j = i; j < x; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }*/
+    }
+}
